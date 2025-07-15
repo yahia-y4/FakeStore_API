87e9ca1504a4720ef5f4c13071c1f6815f7ddcaf
yahia-y4 <yalhmwd994@gmail.com>
2025-07-15 21:44:49 +0300

    commi 2t

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("hello word")
+document.getElementById("add_icon").onclick =()=>{
+    document.getElementById("form_div").style.display="flex";
+}
+
+document.getElementById("x_but").onclick =(e)=>{
+ e.preventDefault();
+    document.getElementById("form_div").style.display="none";
+}
